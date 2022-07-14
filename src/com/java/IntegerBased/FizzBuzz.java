0861d81aa0e0ba084e3086f2b6247d2fc722714e
ArjunKiruthivasan <arjun.kiruthivasan@gmail.com>
2022-07-14 09:03:28 +0530

    Project Setup

---
+++ src/com/java/IntegerBased/FizzBuzz.java
@@ -0,0 +1,5 @@
+package com.java.IntegerBased;
+
+public class FizzBuzz {
+	
+}
---
+++ src/com/java/IntegerBased/IntegerSolutions.java
@@ -0,0 +1,15 @@
+package com.java.IntegerBased;
+
+public class IntegerSolutions {
+	public static void main(String args[]) {
+		/*
+		 * 1. Write a program that outputs the string representation of numbers from 1 to n.
+		 * 
+		 * But for multiple of three it should print "Fizz" instead of the number 
+		 * 
+		 * And for the multiples of "Buzz".
+		 * 
+		 * For numbers which are multiples of both three and five print "FizzBuzz". 
+		 */
+	}
+}
